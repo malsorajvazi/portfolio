7512ac89fbd2bcd4d310617c441f0ca82bac3b02
Malsor Ajvazi <malsorajvazi@gmail.com>
2023-09-05 14:41:48 +0200

    Testimonials and contact sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,7 +106,7 @@ modalCloses.forEach((modalClose) => {
 
 // -----------------------PORTFOLIO SWIPER-------------------//
 
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop:true,
 
@@ -118,4 +118,24 @@ let swiper = new Swiper('.portfolio__container', {
       el: '.swiper-pagination',
       clickable: true,
     },
+  });
+
+// -----------------------TESTIMONIAL SWIPER-------------------//
+
+  let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
   });
